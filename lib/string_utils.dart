92080153be9580a7f4e95903fb1536cda3d79ff0
Alexandre Roux Tekartik <alex@tekartik.com>
2017-10-02 11:58:24 +0200

    fix dependencies on test

--- lib/string_utils.dart
+++ lib/string_utils.dart
@@ -2,46 +2,6 @@ import 'value_utils.dart' as value_utils;
 export 'bool_utils.dart' show parseBool;
 export 'int_utils.dart' show parseInt;
 
-//int parseInt(String text, [int defaultValue = 0]) {
-//  try {
-//    return int.parse(text);
-//  } on FormatException catch (e) {
-//    return defaultValue;
-//  }
-//}
-/*
-
-int parseInt(String value, [int defaultValue = null]) {
-  if (value == null) {
-    return defaultValue;
-  }
-  return int.parse(value, onError: (e) {
-    return defaultValue;
-  });
-}
-
-// handle false/true FALSE/TRUE, 0/1
-bool parseBool(String value, [bool defaultValue = false]) {
-  if (value != null) {
-    switch (value) {
-      case '0':
-      case 'false':
-        return false;
-      case '1':
-      case 'true':
-        return true;
-    }
-
-    switch (value.toLowerCase()) {
-      case 'false':
-        return false;
-      case 'true':
-        return true;
-    }
-  }
-  return defaultValue;
-}
-*/
 bool isEmpty(String text) {
   return ((text == null) || (text.length == 0));
 }
