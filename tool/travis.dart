71ea7fe3e209031134e62c367a81f9223bcfa7f5
alex <alex@tekartik.com>
2020-11-17 22:17:43 +0100

    test: fix travis

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,24 +1,5 @@
-import 'package:process_run/shell.dart';
-import 'package:pub_semver/pub_semver.dart';
-
-Version parsePlatformVersion(String text) {
-  return Version.parse(text.split(' ').first);
-}
+import 'package:dev_test/package.dart';
 
 Future main() async {
-  var shell = Shell();
-
-  await shell.run('''
-  dartanalyzer --fatal-warnings  --fatal-infos lib test tool example
-  dartfmt -w lib test tool example --set-exit-if-changed
-  ''');
-
-  await shell.run('''
-  pub run build_runner test -- -p vm,chrome
-  ''');
-
-  await shell.run('''
-  pub run test -p vm,firefox,chrome
-
-  ''');
+  await packageRunCi('.');
 }
