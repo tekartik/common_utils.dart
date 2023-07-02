f54091e29cf9aea4b29051f3075fa5e67bfedbec
Alexandre Roux <alex@tekartik.com>
2023-07-02 15:09:45 +0200

    remove deprecated model stuff

--- lib/model/model_v2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cv/cv.dart';
-
-export 'package:cv/cv.dart';
-export 'package:cv/src/model.dart' show ModelListBase, ModelBase;
-
-extension ModelCompatExt on Model {
-  ModelEntry? getModelEntry(String key) => getMapEntry(key);
-}
