cd15931a1cfabdf5829961ed6b8f7b20523af8f3
Alexandre Roux <alex@tekartik.com>
2025-03-11 17:02:26 +0100

    fix ci

--- test/io_node_run_test.dart
+++ test/io_node_run_test.dart
@@ -2,11 +2,16 @@
 library;
 
 import 'package:dev_build/shell.dart';
+import 'package:process_run/stdio.dart';
 import 'package:test/test.dart';
 
 Future<void> main() async {
   var nodeIsSupported = (await which('node')) != null;
   test('[dart test -p node]', () async {
+    var sw = Stopwatch()..start();
     await run('dart test  --platform node --compiler dart2js');
-  }, skip: !nodeIsSupported);
+    sw.stop();
+    stdout.writeln('dart test -p node - Elapsed: ${sw.elapsed}');
+  }, skip: !nodeIsSupported, timeout: const Timeout(Duration(minutes: 10)));
+  // 17 seconds on a i9 12900K 64GB RAM
 }
